558b726fc23bec8640ded1e4e21cb0bb2fc78e55
Singa Reddy Dasari <quic_singredd@quicinc.com>
2025-06-05 16:14:44 +0530

    [Gen5]: Add support for gen5_gvm target.
    Added support for gen5_gvm targets
    
    Change-Id: I8ce36c29db74a4e20449106f884c6397ca729ced
    Signed-off-by: Singa Reddy Dasari <quic_singredd@quicinc.com>

--- etc/init.qcom.usb.sh
+++ etc/init.qcom.usb.sh
@@ -58,7 +58,11 @@ if [ "$(getprop ro.build.type)" != "user" ]; then
     else
 	  case "$(getprop ro.baseband)" in
 	      "apq")
-	          setprop persist.vendor.usb.config diag,adb
+		if [ "$target" == "gen5" ]; then
+			setprop persist.vendor.usb.config adb
+		else
+	                setprop persist.vendor.usb.config diag,adb
+		fi
 	      ;;
 	      *)
 	      case "$soc_hwplatform" in
